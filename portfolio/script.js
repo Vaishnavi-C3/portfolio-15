ff55bb7d6efe2e50fdba80e46508e5b11c8c9533
Vaishnavi-C3 <147817190+Vaishnavi-C3@users.noreply.github.com>
2024-07-27 20:35:45 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,7 @@
+/* ========================= typing animation =========================*/
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Software Developer","Data Analyst"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
